5752339c24bb681c41da8fa52e30451aa465246f
Charlie <charlieferris15@gmail.com>
2019-09-09 16:47:10 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
-//Entry point for the logger server
+/**
+@author: LavaTheif
+https://github.com/LavaTheif/LavaLogger/README.md
 
-//Starts up the socket server
+This is the logger server entry point.
+*/
+
+//Entry point for the logger server.  Run this file in node.js
+
+//Starts up the socket server and initialise some variables
 let net = require('net');
 let server = net.createServer();
 let logger = require("./logger");
@@ -8,18 +15,21 @@ let logger = require("./logger");
 const fs = require('fs');
 let config = JSON.parse(fs.readFileSync('./config.json'));
 
+//With mongoose, we need to initialise the database, which we can just save in the config temporarily.
 if(config.db_type === 'mongoose'){
 	(async () => {
 		config = await require('./database/init_mongo.js').exec(config);
 	})();
 }
 
+//basic server stuff to set up a socket.
 server.on('connection',function(socket){
     socket.setEncoding('utf8');
 
     socket.setTimeout(100);
 
     socket.on('data',function(data){
+	//when the socket recieves data, save it to the database using the logger interface.
         logger.log(data, config);
     });
 
@@ -43,5 +53,6 @@ server.on('listening',function(){
     console.log('Server is listening!');
 });
 
+//change this so more clients can send their logs.
 server.maxConnections = 15;
 server.listen(config.server_port);
